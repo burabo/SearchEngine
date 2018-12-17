63ee2192e5b72be73f06fa1efbf27da404a10a3f
Martim Lopes Moura <demoniangel97@gmail.com>
2018-12-17 13:37:34 +0000

    Search bar dev iteration

--- src/main/java/SearchBar.java
+++ src/main/java/SearchBar.java
@@ -1,22 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
-public class Demo {
+
+public class SearchBar extends Thread {
+
+    String sharedString;
 
     JFrame frame = new JFrame("Barra de pesquisa");
 
-    public Demo() {
+    @Override
+    public void run() {
 
         frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
 
-        frame.add(new JTextField(20));
+        JTextField text = new JTextField("Texto aqui",20);
+
+        frame.add(text);
         frame.setVisible(true);
+
+        sharedString = text.getText();
     }
 
-    public static void main(String[] args) {
-        new Demo();
-    }
+
 }
-
